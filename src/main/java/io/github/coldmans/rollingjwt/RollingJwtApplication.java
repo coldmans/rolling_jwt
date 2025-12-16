adb02eff10b535a916577c36da74418dd98bbfb3
jinhyeong jang <jinhinjala26@naver.com>
2025-12-16 19:30:21 +0900

    set up

---
+++ src/main/java/io/github/coldmans/rollingjwt/RollingJwtApplication.java
@@ -0,0 +1,13 @@
+package io.github.coldmans.rollingjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollingJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RollingJwtApplication.class, args);
+    }
+
+}
